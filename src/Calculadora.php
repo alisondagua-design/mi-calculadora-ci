d4b4f5344f92831335b84aeb1f93343a9be27eff
Franciscosararat-afk <francisco.ararat@correounivalle.edu.co>
2026-06-03 17:06:53 -0500

    Agrega modulo, porcentaje, maximo, minimo y factorial

--- src/Calculadora.php
+++ src/Calculadora.php
@@ -20,18 +20,53 @@ class Calculadora {
         }
         return $a / $b;
     }
-      public function potencia($base, $exponente) {
+
+    public function potencia($base, $exponente) {
         return pow($base, $exponente);
     }
- 
+
     public function raizCuadrada($a) {
         if ($a < 0) {
-            throw new \InvalidArgumentException("No se puede calcular la raiz cuadrada de un numero negativo.");
+            throw new \InvalidArgumentException(
+                "No se puede calcular la raiz cuadrada de un numero negativo."
+            );
         }
         return sqrt($a);
     }
- 
+
     public function valorAbsoluto($a) {
         return abs($a);
+    }
+
+    public function modulo($a, $b) {
+        return $a % $b;
+    }
+
+    public function porcentaje($total, $porcentaje) {
+        return ($total * $porcentaje) / 100;
+    }
+
+    public function maximo($a, $b) {
+        return max($a, $b);
+    }
+
+    public function minimo($a, $b) {
+        return min($a, $b);
+    }
+
+    public function factorial($n) {
+        if ($n < 0) {
+            throw new \InvalidArgumentException(
+                "El factorial no existe para numeros negativos."
+            );
+        }
+
+        $resultado = 1;
+
+        for ($i = 2; $i <= $n; $i++) {
+            $resultado *= $i;
+        }
+
+        return $resultado;
     }
 }
--- tests/CalculadoraTest.php
+++ tests/CalculadoraTest.php
@@ -30,8 +30,7 @@ class CalculadoraTest extends TestCase {
         $this->calc->dividir(5, 0);
     }
 
-
-     public function testPotencia() {
+ public function testPotencia() {
         $this->assertEquals(8, $this->calc->potencia(2, 3));
     }
  
@@ -47,5 +46,31 @@ class CalculadoraTest extends TestCase {
     public function testValorAbsoluto() {
         $this->assertEquals(5, $this->calc->valorAbsoluto(-5));
     }
-
+    public function testModulo() {
+    $this->assertEquals(1, $this->calc->modulo(7, 3));
 }
+
+public function testPorcentaje() {
+    $this->assertEquals(20, $this->calc->porcentaje(200, 10));
+}
+
+public function testMaximo() {
+    $this->assertEquals(10, $this->calc->maximo(10, 5));
+}
+
+public function testMinimo() {
+    $this->assertEquals(5, $this->calc->minimo(10, 5));
+}
+
+public function testFactorial() {
+    $this->assertEquals(120, $this->calc->factorial(5));
+}
+
+public function testFactorialNegativoLanzaExcepcion() {
+    $this->expectException(\InvalidArgumentException::class);
+    $this->calc->factorial(-1);
+}
+}
+
+ 
+   
